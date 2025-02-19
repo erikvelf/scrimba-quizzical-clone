9325d39c63659ef89b84f07664d739c999947438
Erik Velf <velf.erik@protonmail.com>
2025-02-19 19:00:09 +0100

    Add description at Start

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -2,11 +2,14 @@ export const Start = ({ onStart }) => {
     const styles = {
         title: {
             display: "block",
-            color: "red"
+        },
+        description: {
+            display: "block",
         }
     }
-    return (<>
-        <span style={styles.title}>{"Quizzical"}</span>
+    return (<div>
+        <span style={styles.title}>Quizzical</span>
+        <span style={styles.description}>Answer various questions</span>
         <button onClick={onStart}>Start app</button>
-    </>)
+    </div>)
 }
