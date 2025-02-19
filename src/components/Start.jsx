265313011153521af09d29c1c6100766ac342201
Erik Velf <velf.erik@protonmail.com>
2025-02-19 18:55:38 +0100

    Add title to Start of the app

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,5 +1,12 @@
 export const Start = ({ onStart }) => {
+    const styles = {
+        title: {
+            display: "block",
+            color: "red"
+        }
+    }
     return (<>
+        <span style={styles.title}>{"Quizzical"}</span>
         <button onClick={onStart}>Start app</button>
     </>)
 }
