87d473f6edeaa3171f1880c1bd0cc7eb1fa8f93c
Erik Velf <velf.erik@protonmail.com>
2025-02-22 19:04:45 +0100

    Add tokens to centralize theme

---
+++ shared/tokens.js
@@ -0,0 +1,9 @@
+
+export const Colors = {
+    white: "#f5f7fb",
+    primary: "#4c5a9f",
+    primarySelected: "#d5daf4",
+    primaryDark: "#273364",
+    greenLight: "#f5f7fb",
+    redLight: "#f6d9db"
+}
