70352881acd004fd1c16fb66fb896691dcbff5f3
Erik Velf <velf.erik@protonmail.com>
2025-02-23 16:49:24 +0100

    Add more tokens for answers colors

--- shared/tokens.js
+++ shared/tokens.js
@@ -3,7 +3,9 @@ export const Colors = {
     white: "#f5f7fb",
     primary: "#4c5a9f",
     primarySelected: "#d5daf4",
+    secondary: "#a3abcd",
     primaryDark: "#273364",
+    green: "#94d7a2",
     greenLight: "#f5f7fb",
     redLight: "#f6d9db"
 }
