8fa11871f341df03b0ecec1d8ea5ed214fea9f54
Erik Velf <velf.erik@protonmail.com>
2025-02-28 22:13:18 +0100

    Moved Score styles in inline styles

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Colors } from "../../shared/tokens";
 
 export function Score({score, numberOfQuestions}) {
 
+    const styles = {
+        score: {
+            fontWeight: 600,
+            fontSize: "24px",
+            color: Colors.violetBlack,
+        }
+    }
+
     return (
-        <p id="score">
+        <p style={styles.score}>
             Your score is: {score}/{numberOfQuestions}
         </p>
     )
