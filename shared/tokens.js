6ef7ef423130d667cea1e0680fd90b66475d53d1
Erik Velf <velf.erik@protonmail.com>
2025-02-28 22:10:44 +0100

    Add violetBlack for text color in tokens.js

--- shared/tokens.js
+++ shared/tokens.js
@@ -1,11 +1,11 @@
-
 export const Colors = {
-    white: "#f5f7fb",
-    primary: "#4c5a9f",
-    primarySelected: "#d5daf4",
-    secondary: "#a3abcd",
-    primaryDark: "#273364",
-    green: "#94d7a2",
-    greenLight: "#bbefc6",
-    redLight: "#f6d9db"
-}
+	white: "#f5f7fb",
+	primary: "#4c5a9f",
+	primarySelected: "#d5daf4",
+	secondary: "#a3abcd",
+	primaryDark: "#273364",
+	green: "#94d7a2",
+	greenLight: "#bbefc6",
+	redLight: "#f6d9db",
+	violetBlack: "#273364",
+};
